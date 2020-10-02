f5cfafd2929e13c79e5ffdfc155cc8f1a16b44a7
sasor <s.leamsi99@gmail.com>
2020-10-02 18:17:39 -0400

    uso basico de redux
    Se hace uso del unico action, reducer y store

--- src/raw/redux.js
+++ src/raw/redux.js
@@ -1,1 +1,34 @@
-console.log('hello');
+const { createStore } = require("redux");
+
+// Actions TYPES
+const BUY_POKEMON = 'BUY_POKEMON';
+
+// Actions
+const buy_pokemon_action = (quantity) => {
+    return {
+        type: BUY_POKEMON,
+        payload: quantity
+    }
+}
+// Reducers
+const default_games_state = {
+    pokemon: 10
+};
+
+const games_reducer = (state = default_games_state, action) => {
+    switch (action.type) {
+        case BUY_POKEMON: {
+            return {
+                ...state,
+                pokemon: state.pokemon - action.payload
+            };
+        }
+        default: return state;
+    }
+}
+
+// Store
+const store = createStore(games_reducer);
+console.log('Estado inicial: ', store.getState());
+store.subscribe(_ => console.log('Cambio de estado: ', store.getState())); // listener que escucha cuando cambia de estado
+store.dispatch(buy_pokemon_action(3)); // ejecutando el action de comprar pokemon
